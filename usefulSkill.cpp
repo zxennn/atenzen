b68b2b42c2b8c8a7200f066f6b497c1a6e3b8f4a
zxennn <58060319+zxennn@users.noreply.github.com>
2023-03-27 15:16:23 +0800

    Update usefulSkill.cpp

--- usefulSkill.cpp
+++ usefulSkill.cpp
@@ -1,3 +1,5 @@
+######  1  ########
+
 #include <stdio.h>
 #include <iostream>
 
@@ -23,5 +25,64 @@ int main()
     int result = myFunc(a,b);
 
     std::cout << "result is " << result << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+######  2  ########
+
+
+#include <iostream>
+
+class Complex {
+public:
+    Complex(double r = 0.0, double i = 0.0): real(r), imag(i) {}
+
+    // 重载加法操作符 "+"
+    Complex operator+(const Complex& c) const {
+        return Complex(real + c.real, imag + c.imag);
+    }
+
+    // 重载减法操作符 "-"
+    Complex operator-(const Complex& c) const {
+        return Complex(real - c.real, imag - c.imag);
+    }
+
+    // 重载乘法操作符 "*"
+    Complex operator*(const Complex& c) const {
+        return Complex(real * c.real - imag * c.imag, real * c.imag + imag * c.real);
+    }
+
+    // 重载输出流操作符 "<<"
+    friend std::ostream& operator<<(std::ostream& os, const Complex& c) {
+        os << "(" << c.real << ", " << c.imag << ")";
+        return os;
+    }
+
+private:
+    double real;
+    double imag;
+};
+
+int main() {
+    Complex a(1.0, 2.0);
+    Complex b(3.0, 4.0);
+
+    // 使用重载的加法操作符
+    Complex c = a + b;
+    std::cout << "a + b = " << c << std::endl;
+
+    // 使用重载的减法操作符
+    Complex d = a - b;
+    std::cout << "a - b = " << d << std::endl;
+
+    // 使用重载的乘法操作符
+    Complex e = a * b;
+    std::cout << "a * b = " << e << std::endl;
+
     return 0;
 }
